321e1b415e2f8d9a9173e2f5e99db88d51ee5ea3
KillerMR <killermbjustthink@gmail.com>
2022-06-21 15:19:29 +0300

    Easiest example

---
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -0,0 +1,9 @@
+num1 = 13
+num2 = 10
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        summary = num1 + num2
+        return summary
+    
+sum = Solution()
+print(sum.sum(num1, num2))
