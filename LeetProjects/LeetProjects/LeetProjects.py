7973fd64a730e6ebbf48e264436816a7fe42c30e
KillerMR <killermbjustthink@gmail.com>
2022-06-22 15:37:42 +0300

    Example: Build Array from Permutation

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,7 +1,14 @@
-numbers = [1,2,3]
+numbers = [4,2,3,3,1,5]
 class Solution:
-    def getConcatenation(self, nums: list[int]) -> list[int]:
-        ans = nums + nums
-        return ans
-answ = Solution()
-print(answ.getConcatenation(numbers))
+    def buildarray(self, nums: list[int]) -> list[int]:
+        ans = [0] * len(nums)
+        if(min(nums) >= 0 and max(nums) <= len(nums) - 1):
+            i = 0
+            while(i < len(nums)):
+                ans[i] = nums[nums[i]]
+                i += 1
+            return ans
+        else:
+            return "Array isn't correct"
+sol = Solution()
+print(sol.buildarray(numbers))
