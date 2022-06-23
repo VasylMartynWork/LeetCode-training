7e7230d04f06031eff520045560d9030c1dfc1ac
KillerMR <killermbjustthink@gmail.com>
2022-06-23 19:04:20 +0300

    Example(771): Jewels and Stones

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,18 +1,12 @@
-numbers = [1,2,3]
+jewel = "z"
+stone = "ZZ"
 class Solution:
-    def numIdenticalPairs(self, nums: list[int]) -> int:
-        i = 0
-        j = 0
-        countOfPairs = 0
-        while(i < len(nums)):
-            j = 0
-            while(j < len(nums)):
-                if(nums[i] == nums[j] and i < j):
-                    countOfPairs += 1
-                j += 1
-            i += 1
-        return countOfPairs
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        numJew = 0
+        for jew in jewels:
+            numJew += stones.count(jew)
+        return numJew
 
 sol = Solution()
-print(sol.numIdenticalPairs(numbers))
+print(sol.numJewelsInStones(jewel, stone))
         
