3ecded4c078a7b11d41d856ff58b7eba87edea77
KillerMR <killermbjustthink@gmail.com>
2022-06-23 13:44:03 +0300

    Example(1470): Shuffle the Array

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,10 +1,17 @@
-account = [[1,5],[7,3],[3,5]]
+nums = [2,5,1,3,4,7]
+d = 3
+
 class Solution:
-    def maximumWealth(self, accounts: list[list[int]]) -> int:
-        richest = 0
-        for acc in accounts:
-            if(sum(acc) > richest): richest = sum(acc)
-        return richest
+    def shuffle(self, nums: list[int], n: int) -> list[int]:
+        nums1 = nums[:n]
+        nums2 = nums[n:2*n]
+        shuffled = []
+        i = 0
+        while(i < len(nums1)):
+            shuffled.append(nums1[i])
+            shuffled.append(nums2[i])
+            i += 1
+        return shuffled
 
 sol = Solution()
-print(sol.maximumWealth(account))
+print(sol.shuffle(nums, d))
