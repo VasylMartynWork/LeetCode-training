8491a874ce3c8c924b50d9969fc0284a0565d9a4
KillerMR <killermbjustthink@gmail.com>
2022-06-24 09:34:51 +0300

    Example(1431): Kids With the Greatest Number of Candies

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,12 +1,25 @@
-jewel = "z"
-stone = "ZZ"
+candie = [4,2,1,1,2]
+extraCandie = 1
+
 class Solution:
-    def numJewelsInStones(self, jewels: str, stones: str) -> int:
-        numJew = 0
-        for jew in jewels:
-            numJew += stones.count(jew)
-        return numJew
+    def kidsWithCandies(self, candies: list[int], extraCandies: int) -> list[bool]:
+        n = len(candies)
+        result = [0] * n
+        newCandie = [0] * n
+        i = 0
+        j = 0
+        while(i < n):
+            newCandie[i] = candies[i] + extraCandies
+            j = 0
+            while(j < n):
+                if(newCandie[i] > candies[j] or newCandie[i] == candies[j]):
+                    result[i] = True
+                else:
+                    result[i] = False
+                    break
+                j += 1
+            i += 1
+        return result
 
 sol = Solution()
-print(sol.numJewelsInStones(jewel, stone))
-        
+print(sol.kidsWithCandies(candie, extraCandie))
