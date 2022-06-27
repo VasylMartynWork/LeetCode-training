960addd995fc173eec7dc8b21c46732464fdadd3
KillerMR <killermbjustthink@gmail.com>
2022-06-27 10:54:18 +0300

    Example(1859): Sorting the Sentence

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,24 +1,18 @@
-st = "P7:Z7"
+st = "Myself2 Me1 I4 and3"
 class Solution:
-    def cellsInRange(self, s: str) -> list[str]:
-        alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
-                    "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        stri = s.split(":")
-        des = int(stri[1][1]) - int(stri[0][1])
-        i = alphabet.index(stri[0][0])
-        res = []
-        while(i <= alphabet.index(stri[1][0])):
-            j = int(stri[0][1])
-            while(j <= int(stri[1][1])):
-                stro = alphabet[i]
-                stro += str(j)
-                res.append(stro)
-                j += 1
-            if(des == 0):
-                i += des + 1
-            else:
-                i += 1
-        return res
+    def sortSentence(self, s: str) -> str:
+        stri = s.split()
+        st = ""
+        i = 1
+        while(i <= len(stri)):
+            for el in stri:
+                if(int(el[len(el) - 1]) == i):
+                    st += el
+                    st = st.replace(f'{i}', '')
+                    if(i < len(stri)):
+                        st += " "
+            i += 1
+        return st
 
 sol = Solution()
-print(sol.cellsInRange(st))
+print(sol.sortSentence(st))
