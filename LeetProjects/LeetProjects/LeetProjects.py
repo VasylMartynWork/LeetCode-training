8c98880dfa665c55798c26b9246ec444a19739ae
KillerMR <killermbjustthink@gmail.com>
2022-06-27 18:00:57 +0300

    Example(1588): Sum of All Odd Length Subarrays

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,17 +1,21 @@
-n = 5
-start = 0
+arr = [1,4,2,5,3]
 
 class Solution:
-    def xorOperation(self, n: int, start: int) -> int:
-        nums = []
-        i = 0
-        xor = 0
-        while(i < n):
-            nums.append(start + 2 * i)
-            xor = xor ^ nums[i]
-            i += 1
-
-        return xor
+    def sumOddLengthSubarrays(self, arr: list[int]) -> int:
+        j = 1
+        res = 0
+        while True:
+            if(j > len(arr)):
+                break
+            i = 0
+            while(i < len(arr)):
+                if(len(arr[i:i + j]) < j):
+                    break
+                a = arr[i:i + j]
+                res += sum(a)
+                i += 1
+            j += 2
+        return res
 
 sol = Solution()
-print(sol.xorOperation(n, start))
+print(sol.sumOddLengthSubarrays(arr))
