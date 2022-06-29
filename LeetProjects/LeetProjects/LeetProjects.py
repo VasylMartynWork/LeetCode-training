12186479868ba95e119559a35a4ca85c1036ee17
KillerMR <killermbjustthink@gmail.com>
2022-06-29 10:47:18 +0300

    Example(2006): Count Number of Pairs With Absolute Difference K

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,29 +1,14 @@
-n = 14
+nums = [1,3]
+k = 3
 
 class Solution:
-    def numberOfMatches(self, n: int) -> int:
-        team = n
-        roun = 0
-        if n % 2 == 0:
-            while True:
-                if(team == 1):
-                    break
-                if(team % 2 == 1):
-                    roun += (team - 1) // 2
-                    team = (team - 1) // 2 + 1
-                else:
-                    roun += team // 2
-                    team = team // 2
-        else:
-            while True:
-                if(team == 1):
-                    break
-                if(team % 2 == 1):
-                    roun += (team - 1) // 2
-                else:
-                    roun += team // 2
-                team = (team - 1) // 2 + 1
-        return roun
+    def countKDifference(self, nums: list[int], k: int) -> int:
+        co = 0
+        for i in nums:
+            for j in nums:
+                if(i < j and abs(i - j) == k):
+                    co += 1
+        return co
 
 sol = Solution()
-print(sol.numberOfMatches(n))
+print(sol.countKDifference(nums, k))
