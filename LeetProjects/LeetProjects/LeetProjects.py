ae140e4af3764fb97d22dc8c8e7fdecb2c34c46b
KillerMR <killermbjustthink@gmail.com>
2022-07-01 10:10:04 +0300

    Example(1832): Check if the Sentence Is Pangram

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,22 +1,18 @@
-rings = "G4"
+sentence = "mqzedwtwcdopqehzhcbphiwqpnmlmyzabqpfvigmyborlzcasgivklcuwbiilohvzysfmodeefagnmkexnaridkgxrhzedlsitbsetaofosptvcklkaqxasrcbmlabvwhzmwdglfkmqkkqdxucbwsizgiuchgeptmgslrvxzgaunoiifnlsltubxlbfnsxvyzhuqszvebvtcqurwnacsfavoupiqmbfwmuyingsfwbernknanqpyxkocstdtbxymvrdecnyffhipwlhyonvbrlqicdomgexguyvldoeazagmwxrnxwzifrcklotrbuoqiefotlkevubpguuicvgucupegeswtcgzubzihqtfvhxdrxxnudiccqhtqucoogpdekbosxacfvtlmvqowvwqteimsisnqvxikonvspfkheruovqmkbfnefqtduynoqydkmgopwwtloshegflixsihomiccfofgqbghqxxsyuedgdnahaqbazhxxtsfbadwxaxhcnxwspmkxwbazqxpkrghoegwbiblwwkmgzbymsvmnrebgrzkpwunxtkruuenxguckaxqmvkstdkexlvaslalfwxivrswhgrvhknzwcmueqqlxgkklbmefchcdtxqcwarkvwwqulfmofnsvmrbxxlsaiqnzxrsffapqwpllndqnsmbgseaeipmrthgwrnrplgufzwahkzxhckzidiokttoclvvmowtreafspznomsouqzttspvoxtkzmxacnmxhzfhhtyrbgeroloeoncpikhktfmfhueahppzeekfaqvtslivpvlmuskdzrofdtgpnmpfflsymkggpraulvpffkbfvrxdhirubsidzfwvwbriifntgdmkuodwhkbmqxkkagcooitylwqgxqvmnoultivwpmilorbtwpcxzgbtxpzhqhuqhzmtqyfpbinmktabcwisquhn"
 
 class Solution:
-    def countPoints(self, rings: str) -> int:
-        rg = ["R", "G", "B"]
-        i = 0
-        res = 0
-        while(i < 10):
-            co = 0
-            for j in rg:
-                exist = f"{j}{i}" in rings
-                if(exist == True):
-                    co += 1
-                else:
-                    co = 0
-            if(co == 3):
-                res += 1
-            i += 1
-        return res
+    def checkIfPangram(self, sentence: str) -> bool:
+        alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+                "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        co = 0
+        for i in alph:
+            exist = i in sentence
+            if(exist == True):
+                co = 1
+            else:
+                return False
+        if(co == 1):
+            return True
 
 sol = Solution()
-print(sol.countPoints(rings))
+print(sol.checkIfPangram(sentence))
