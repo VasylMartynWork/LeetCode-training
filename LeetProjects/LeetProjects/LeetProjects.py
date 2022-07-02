2619a7294a18e8fb7d605697f0956c16cff14a42
KillerMR <killermbjustthink@gmail.com>
2022-07-02 11:22:29 +0300

    Example(832): Flipping an Image

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,19 +1,20 @@
-nums = [3,1,2,2,2,1,3]
-k = 2
+image = [[1,1,0],[1,0,1],[0,0,0]]
 
 class Solution:
-    def countPairs(self, nums: list[int], k: int) -> int:
-        i = 0
-        co = 0
-        while(i < len(nums)):
-            j = 0
-            while(j < len(nums)):
-                if(i < j and nums[i] == nums[j] and (i * j) % k == 0):
-                    co += 1
-                j += 1
-            i += 1
-        return co
+    def flipAndInvertImage(self, image: list[list[int]]) -> list[list[int]]:
+        res = []
+        for i in image:
+            i.reverse()
+            resu = []
+            for j in i:
+                if(j == 1):
+                    j = 0
+                elif(j == 0):
+                    j = 1
+                
+                resu.append(j)
+            res.append(resu)
+        return res
 
 sol = Solution()
-print(sol.countPairs(nums, k))
-
+print(sol.flipAndInvertImage(image))
