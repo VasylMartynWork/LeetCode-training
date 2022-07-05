1e4ab9ed08c1289502bbf7e6ab94342781321504
KillerMR <killermbjustthink@gmail.com>
2022-07-05 11:14:06 +0300

    Example(1725): Number Of Rectangles That Can Form The Largest Square

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,30 +1,13 @@
-s = "10#11#12"
+rectangles = [[2,3],[3,7],[4,3],[3,7]]
 
 class Solution:
-    def freqAlphabets(self, s: str) -> str:
-        i = len(s) - 1
-        resa = []
-        res = ""
-        alph = ["a1", "b2", "c3", "d4", "e5", "f6", "g7", "h8", "i9", "j10#", "k11#", "l12#", "m13#", "n14#", "o15#",
-                 "p16#", "q17#", "r18#", "s19#", "t20#", "u21#", "v22#", "w23#", "x24#", "y25#", "z26#",]
-        while(i >= 0):
-            if(s[i] == "#"):
-                for j in alph:
-                    if(s[i - 2: i] == j[1:3]):
-                        resa += j[0]
-                        i -= 3
-                        break
-            else:
-                for j in alph:
-                    if(s[i] == j[1]):
-                        resa += j[0]
-                        i -= 1
-                        break
-        k = len(resa) - 1
-        while(k >= 0):
-            res += resa[k]
-            k -= 1
-        return res
+    def countGoodRectangles(self, rectangles: list[list[int]]) -> int:
+        res = []
+        for i in rectangles:
+            maxi = min(i)
+            res.append(maxi)
+        maxim = max(res)
+        return res.count(maxim)
 
 sol = Solution()
-print(sol.freqAlphabets(s))
+print(sol.countGoodRectangles(rectangles))
