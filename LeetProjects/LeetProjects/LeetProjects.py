05e0fb9ab44a8ed063a2877ebbf601c35e3f1839
KillerMR <killermbjustthink@gmail.com>
2022-07-11 10:10:51 +0300

    Example(1812): Determine Color of a Chessboard Square

--- LeetProjects/LeetProjects/LeetProjects.py
+++ LeetProjects/LeetProjects/LeetProjects.py
@@ -1,23 +1,13 @@
-s = "eOEvoOAfOIIi"
+coordinates = "h3"
 
 class Solution:
-    def halvesAreAlike(self, s: str) -> bool:
-        vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-        a = s[0:int(len(s) / 2)]
-        b = s[int(len(s) / 2):len(s)]
-        one = 0
-        two = 0
-        for i in vowel:
-            yea = i in a
-            yae = i in b
-            if(yea == True):
-                one += a.count(i)
-            if(yae == True):
-                two += b.count(i)
-        if(one == two):
+    def squareIsWhite(self, coordinates: str) -> bool:
+        alph = ["a", "b", "c", "d", "e", "f", "g", "h"]
+        a = alph.index(coordinates[0]) + 1 + int(coordinates[1])
+        if(a % 2 == 1):
             return True
         else:
             return False
 
 sol = Solution()
-print(sol.halvesAreAlike(s))
+print(sol.squareIsWhite(coordinates))
